8aee446ea6c0074e0558bec2aa99a058a2abecc7
leejihyun-dev <jh_lee@sqisoft.com>
2025-02-19 22:50:09 +0900

    불필요한 코드 삭제

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,11 +1,9 @@
-import 'package:app_looklog/core/config/app_config.dart';
 import 'package:app_looklog/features/home/home_app.dart';
 import 'package:app_looklog/features/home/view/widget/menu_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../common/theme/colors.dart';
-import 'features/home/view/controller/menu_controller.dart';
 
 class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
@@ -17,7 +15,6 @@ class MainApp extends ConsumerStatefulWidget {
 class _MainAppState extends ConsumerState<MainApp> {
   @override
   Widget build(BuildContext context) {
-
     return const Scaffold(
       backgroundColor: BG_COLOR,
       body: Stack(
